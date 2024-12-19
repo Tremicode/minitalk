871b3ab7f082701b6873c2cc5652e29ac1ddd014
Carlos Tremino Fernandez <ctremino@c2r18s6.42madrid.com>
2024-12-19 14:09:43 +0100

    malloc_update

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: ctremino <ctremino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 12:03:35 by ctremino          #+#    #+#             */
-/*   Updated: 2024/12/19 12:05:49 by ctremino         ###   ########.fr       */
+/*   Updated: 2024/12/19 13:56:36 by ctremino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,41 +14,56 @@
 
 static void	ft_send_signal(pid_t pid, int sig)
 {
-	// (only SIGUSR1 y SIGUSR2)
+	// Solo SIGUSR1 y SIGUSR2 están permitidos
 	if (sig != SIGUSR1 && sig != SIGUSR2)
 	{
-		ft_putstr_fd("Error: wrong singnal!!. only SIGUSR1 and SIGUSR2 allowed.\n",
-			1);
+		ft_putstr_fd("Error: wrong signal! Only SIGUSR1 and SIGUSR2 allowed.\n", 1);
 		exit(EXIT_FAILURE);
 	}
 	// Intentar enviar la señal al proceso
 	if (kill(pid, sig) == -1)
 	{
-		ft_putstr_fd("Error al intentar enviar la señal.\n", 1);
+		ft_putstr_fd("Error sending signal.\n", 1);
 		exit(EXIT_FAILURE);
 	}
 }
+
 static void	ft_send_message(pid_t pid, char *msg)
 {
 	unsigned char	current_char;
+	int bit_index;
 
-	int bit_index; // Índice de los bits dentro del carácter (de 7 a 0)
+	// Mientras haya caracteres en el mensaje
 	while (*msg)
 	{
 		current_char = *msg;
-		bit_index = 7; // start bit más significativo (más a la izquierda)
-		// Enviamos 1 bit a la vez, de izquierda a derecha
+		bit_index = 7; // Iniciar desde el bit más significativo (izquierda)
+
+		// Enviar los bits de 8 bits de un carácter
 		while (bit_index >= 0)
 		{
-			// Comprobamos  el bit actual del carácter es 1 o 0
+			// Si el bit está en 1, enviar SIGUSR1, si está en 0, SIGUSR2
 			if (current_char & (1 << bit_index))
-				ft_send_signal(pid, SIGUSR1); // Si el bit es 1, SIGUSR1
+				ft_send_signal(pid, SIGUSR1);
 			else
-				ft_send_signal(pid, SIGUSR2); // Si el bit es 0,  SIGUSR2
-			usleep(1000);
-			bit_index--; // (de izq a derecha)
+				ft_send_signal(pid, SIGUSR2);
+			usleep(10000); // Aumentar el delay para asegurar que las señales se procesen correctamente
+			bit_index--; // Mover al siguiente bit
 		}
 		msg++;
+	}
+
+	// Enviar un caracter '\0' al final del mensaje para indicar su final
+	current_char = '\0';
+	bit_index = 7;
+	while (bit_index >= 0)
+	{
+		if (current_char & (1 << bit_index))
+			ft_send_signal(pid, SIGUSR1);
+		else
+			ft_send_signal(pid, SIGUSR2);
+		usleep(10000); // Mismo retraso para la señal de fin de mensaje
+		bit_index--;
 	}
 }
 
@@ -56,23 +71,28 @@ int	main(int argc, char **argv)
 {
 	pid_t	process_pid;
 
+	// Comprobar número correcto de argumentos
 	if (argc != 3)
 	{
 		write(1, "Wrong number of arguments, try again! \n", 37);
 		return (1);
 	}
+
 	// Convierte el PID de string a int usando atoi
 	process_pid = atoi(argv[1]);
-	// PID tiene que ser un valor positivo (no puede ser cero o negativo)
+	// Comprobar si el PID es válido (debe ser positivo)
 	if (process_pid <= 0)
 	{
 		write(1, "You are not getting a valid PID!\n", 32);
 		return (1);
 	}
+
 	write(1, "PID: ", 5);
 	ft_putnbr_fd(process_pid, 1); // Imprime el valor del PID
 	write(1, "\n", 1);
+
 	// Llama a la función send_msg para enviar el mensaje
 	ft_send_message(process_pid, argv[2]);
 	return (0);
 }
+
--- server.c
+++ server.c
@@ -6,51 +6,69 @@
 /*   By: ctremino <ctremino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 18:36:13 by ctremino          #+#    #+#             */
-/*   Updated: 2024/12/19 12:45:48 by ctremino         ###   ########.fr       */
+/*   Updated: 2024/12/19 14:07:41 by ctremino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+static void	handle_message(char current_char, char **message, int *length)
+{
+	char	*new_message;
+
+	new_message = malloc(*length + 2); // +1 nuevo carácter, +1 '\0'
+	if (!new_message)
+		exit(EXIT_FAILURE);
+	if (*message)
+		memcpy(new_message, *message, *length); // Copiar contenido previo
+	new_message[*length] = current_char;
+	new_message[++(*length)] = '\0';
+	free(*message);
+	*message = new_message;
+}
+
 static void	receive_message(int sig, siginfo_t *info, void *context)
 {
-	static int bits_left = 8;     // Contador de bits restantes
-	static char current_char = 0; // Carácter en construcción
-	(void)info;    // No usamos este parámetro
-	(void)context; // No usamos este parámetro
-	// Si recibimos SIGUSR1, encendemos el bit correspondiente
+	static int	bits_left = 8;
+	static char	current_char = 0;
+	static char	*message = NULL;
+	static int	length = 0;
+
+	(void)info;
+	(void)context;
 	if (sig == SIGUSR1)
 		current_char |= (1 << --bits_left);
 	else
 		bits_left--;
-	// Cuando completamos los 8 bits, imprimimos el carácter
 	if (bits_left == 0)
 	{
-		ft_putchar_fd(current_char, 1);
-		bits_left = 8;    // Reiniciamos contador de bits
-		current_char = 0; // Reiniciamos el carácter
+		if (current_char == '\0') // Fin del mensaje
+		{
+			write(1, message, length);
+			free(message);
+			message = NULL;
+			length = 0;
+		}
+		else
+			handle_message(current_char, &message, &length);
+		current_char = 0;
+		bits_left = 8;
 	}
 }
+
 static void	initsig(void)
 {
 	struct sigaction	sa;
 
-	// Configuramos la función que manejará las señales (receive_message)
+	// Configura la función que manejará las señales (receive_message)
 	sa.sa_sigaction = receive_message;
-	// Indicamos que queremos usar información adicional sobre la señal
 	sa.sa_flags = SA_SIGINFO;
-	// Limpiamos el conjunto de señales para no bloquear ninguna durante el manejo
 	sigemptyset(&sa.sa_mask);
-	// Registramos el manejador de señales para SIGUSR1
-	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+	// Registra los manejadores para SIGUSR1 y SIGUSR2
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) ==
+		-1)
 	{
-		ft_putstr_fd("Error configuring SIGUSR1\n", 1);
-		exit(EXIT_FAILURE);
-	}
-	// Registramos el manejador de señales para SIGUSR2
-	if (sigaction(SIGUSR2, &sa, NULL) == -1)
-	{
-		ft_putstr_fd("Error configuring SIGUSR2\n", 1);
+		ft_putstr_fd("Error configuring signals\n", 1);
 		exit(EXIT_FAILURE);
 	}
 }
@@ -62,14 +80,14 @@ int	main(void)
 	process_pid = getpid();
 	if (process_pid <= 0)
 	{
-		ft_putstr_fd("wrong PID please try again! \n", 1);
+		ft_putstr_fd("Error: Invalid PID\n", 1);
 		return (1);
 	}
 	initsig();
 	write(1, "PID: ", 5);
-	ft_putnbr_fd(process_pid, 1); // Imprime el valor del PID
+	ft_putnbr_fd(process_pid, 1); // Imprime el PID del proceso
 	write(1, "\n", 1);
 	while (1)
-		pause();
+		pause(); // Espera señales indefinidamente
 	return (0);
 }
